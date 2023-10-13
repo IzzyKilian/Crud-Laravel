d37fef0065bb469a8822da3eb61caa71120f021e
IzzyKilian <128013525+IzzyKilian@users.noreply.github.com>
2023-10-13 11:20:10 +0800

    Add files via upload

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mahasiswa;
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $mahasiswa = Mahasiswa::orderBy('created_at', 'DESC')->get();
+
+        return view('pages.mahasiswa.index',compact('mahasiswa'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('pages.mahasiswa.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        
+        Mahasiswa::create($request->all());
+        return redirect()->route('mahasiswa.index')->with('success', 'Data berhasil ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $mahasiswa = Mahasiswa::findOrFail($id);
+
+        return view('pages.mahasiswa.show', compact('mahasiswa'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $mahasiswa = Mahasiswa::findOrFail($id);
+
+        return view('pages.mahasiswa.edit', compact('mahasiswa'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $mahasiswa = Mahasiswa::findOrFail($id);
+
+        $mahasiswa->update($request->all());
+
+        return redirect()->route('mahasiswa.index')->with('success', 'Data updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $mahasiswa = Mahasiswa::findOrFail($id);
+
+        $mahasiswa->delete();
+
+        return redirect()->route('mahasiswa.index')->with('success', 'Data deleted successfully');
+    }
+}
